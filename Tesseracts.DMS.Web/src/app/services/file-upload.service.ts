93209293a3819f8a1de78a4e33a3bad2351a65a1
Nixon Chakiath <nich04@group.gac>
2018-10-18 15:58:20 +0400

    removed commented code

--- Tesseracts.DMS.Web/src/app/services/file-upload.service.ts
+++ Tesseracts.DMS.Web/src/app/services/file-upload.service.ts
@@ -16,27 +16,4 @@ export class FileUploadService {
     this.httpClient.get("http://localhost:49234/api/master/GetAllDocumentTags").
     subscribe(tags => {this.documentTags = tags as DocumentTag[]});
   }
-
-  // uploadFile(droppedFile:UploadFile, file: File){
-
-  //   // console.log(file);
-  //   // console.log(droppedFile);
-          
-  //     // You could upload it like this:
-  //     const formData = new FormData();
-  //     formData.append('file', file, file.name);
-
-  //     // Headers
-  //     const headers = new HttpHeaders({
-  //       'security-token': 'mytoken'
-  //     })
-
-  //     this.httpClient.post('"http://localhost:49234/api/master/UploadFile', formData, 
-  //     //{ headers: headers, responseType: 'blob' }
-  //     ).subscribe(data => {
-  //       // Sanitized logo returned from backend
-  //       console.log(data);
-  //     }) 
-  // }
-
 }
